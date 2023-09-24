17f199fd14b45a8f5db666cd92cad001dfd928ce
rasmus123d <59487370+RasmusKD@users.noreply.github.com>
2023-09-24 23:18:44 +0200

    Create Kap6Opgave6.java

---
+++ src/Kap6Opgave6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Kap6Opgave6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        displayPattern(n);
+    }
+    public static void displayPattern(int n) {
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
